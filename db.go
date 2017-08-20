482f5a3c60c52a9682cd17195457a7c16b2125a3
Khusbu Mishra <meetkhusbumihsra@gmail.com>
2017-08-20 13:32:39 +0530

    Fetch API added along with error handling

--- db.go
+++ db.go
@@ -1,8 +1,43 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/jinzhu/gorm"
+)
+
+var (
+	dbc *gorm.DB
+)
+
 // CreateTables creates tables for each model
 func CreateTables() {
 	dbc.CreateTable(&Company{}, &FundingDetails{})
 	dbc.Model(&FundingDetails{}).AddForeignKey("profile_id", "companies(profile_id)", "RESTRICT", "RESTRICT")
 	dbc.AutoMigrate(&Company{}, &FundingDetails{})
+}
+
+// CreateRecord creates a record in the database
+func CreateRecord(model interface{}) error {
+	return dbc.Create(model).Error
+}
+
+// FetchRecord fetches a record from the database having profile_id `id`
+func FetchRecord(id string) (Company, error) {
+	var company Company
+	if err := dbc.Preload("FundingDetails").Where("profile_id = ?", id).First(&company).Error; err != nil {
+		return company, err
+	}
+	return company, nil
+}
+
+// SetUpDB establishes a connection to the database and create tables
+func SetUpDB() {
+	var err error
+	dbc, err = gorm.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("Error opening database: %q", err)
+	}
+	CreateTables()
 }
--- handlers.go
+++ handlers.go
@@ -1,24 +1,38 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 )
 
 // Create creates a new record in the database
 func Create(c *gin.Context) {
 	company := Company{}
 	company.SetProfileID()
-	company.FundingDetails = &FundingDetails{}
-	dbc.Create(company)
+	company.FundingDetails = &FundingDetails{Amount: 123}
+	if err := CreateRecord(company); err != nil {
+		log.Printf("Error creating record: %q", err)
+		c.JSON(http.StatusInternalServerError, nil) //TODO pass some data
+	}
 	c.JSON(http.StatusOK, company)
 }
 
 // Fetch fetches company details from the database with the requested profile_id
 func Fetch(c *gin.Context) {
 	profileID := c.Param("profile_id")
-	var company Company
-	dbc.Preload("FundingDetails").Where("profile_id = ?", profileID).First(&company)
+	company, err := FetchRecord(profileID)
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			log.Printf("Record not found: %s", profileID)
+			c.JSON(http.StatusNotFound, nil)
+			return
+		}
+		log.Printf("Error fetching record: %q", err)
+		c.JSON(http.StatusInternalServerError, nil) //TODO pass some data
+		return
+	}
 	c.JSON(http.StatusOK, company)
 }
